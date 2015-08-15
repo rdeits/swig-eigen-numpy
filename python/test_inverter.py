c9f1bee57a233ff2dceb234c52d938c1311f5687
Robin Deits <robin.deits@gmail.com>
2015-08-15 17:52:08 -0400

    add an error test

--- python/test_inverter.py
+++ python/test_inverter.py
@@ -6,3 +6,8 @@ m = np.array([[2.0, 0.0],
               [0.0, 2.0]])
 print inv.getInverse(m)
 print inv.getInversePlus1(m)
+
+try:
+    inv.getInverse("foo")
+except Exception as e:
+    print "Caught an expected error:", e
