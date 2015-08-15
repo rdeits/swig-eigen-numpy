bf5a71e2cfedec9f3968f02d63fdae063ca5ebf6
Robin Deits <robin.deits@gmail.com>
2015-08-15 17:35:05 -0400

    clean up

--- python/pyinverter/__init__.py
+++ python/pyinverter/__init__.py
@@ -1,7 +1,1 @@
-from inverter_wrapper import Inverter
-import numpy as np
-
-def Inverter_getInversePlus1(self, M):
-    inv = self.getInverse(M)
-    return inv + np.ones(M.shape)
-setattr(Inverter, "getInversePlus1", Inverter_getInversePlus1)
+from pyinverter import Inverter
