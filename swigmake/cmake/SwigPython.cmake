82846c06d5a89f16b3ec093796d20648292df9fe
Robin Deits <robin.deits@gmail.com>
2015-08-20 17:51:53 -0400

    collect cmake and swig files together

--- swigmake/cmake/SwigPython.cmake
+++ swigmake/cmake/SwigPython.cmake
@@ -1,11 +1,19 @@
 include(CMakeParseArguments)
 
 function(add_swig_python_module)
+	# Parse our arguments and make sure we got the required ones
 	set(options CPLUSPLUS)
 	set(oneValueArgs SWIG_I_FILE TARGET)
 	set(multiValueArgs INCLUDE_DIRS LINK_LIBRARIES SWIG_INCLUDE_DIRS)
 	cmake_parse_arguments(swigpy "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+	if (NOT swigpy_TARGET)
+		message(FATAL_ERROR "Error using add_swig_python_module: Please provide a target name with 'TARGET targetname'")
+	endif()
+	if (NOT swigpy_SWIG_I_FILE)
+		message(FATAL_ERROR "Error using add_swig_python_module: Please provide the full path to your .i file with 'SWIG_I_FILE filepath'")
+	endif()
 
+	# Find python and get its version number
 	find_package(PythonInterp REQUIRED)
 	set(PYVERSION "${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}")
 
@@ -29,7 +37,7 @@ function(add_swig_python_module)
 	find_package(SWIG REQUIRED)
 	include(UseSWIG)
 
-	# Find the numpy header paths and include them. This calls the cmake/FindNumPy.cmake file included in this repo. 
+	# Find the numpy header paths and include them. This calls the FindNumPy.cmake file included in this repo. 
 	find_package(NumPy REQUIRED)
 	include_directories(${NUMPY_INCLUDE_DIRS})
 
@@ -37,6 +45,7 @@ function(add_swig_python_module)
 	find_package( PythonLibs REQUIRED )
 	include_directories( ${PYTHON_INCLUDE_DIRS} )
 
+	# Include any source directories that swig will need to find our c++ header files
 	foreach(dir IN LISTS swigpy_INCLUDE_DIRS)
 		include_directories(${dir})
 	endforeach(dir)
@@ -57,17 +66,15 @@ function(add_swig_python_module)
 			CPLUSPLUS ${CPLUSPLUS})
 	endif()
 
+	# Tell swig to also look for .i interface files in these folders
 	foreach(dir IN LISTS swigpy_SWIG_INCLUDE_DIRS)
 		set(CMAKE_SWIG_FLAGS ${CMAKE_SWIG_FLAGS} "-I${dir}")
 	endforeach(dir)
-	# foreach(dir IN LISTS swigpy_INCLUDE_DIRS)
-	# 	set(CMAKE_SWIG_FLAGS ${CMAKE_SWIG_FLAGS} "-I${dir}")
-	# endforeach(dir)
 
-	message("swigpy_TARGET: " ${swigpy_TARGET})
-	message("swigpy_SWIG_I_FILE: " ${swigpy_SWIG_I_FILE})
+	# Tell swig to build python bindings for our target library and link them against the C++ library. 
 	swig_add_module(${swigpy_TARGET} python ${swigpy_SWIG_I_FILE})
 	swig_link_libraries(${swigpy_TARGET} ${swigpy_LINK_LIBRARIES} ${PYTHON_LIBRARIES})
 
-	set(${SWIG_MODULE_${swigpy_TARGET}_REAL_NAME} ${SWIG_MODULE_${swigpy_TARGET}_REAL_NAME} PARENT_SCOPE)
+	# Set a variable in the scope of the cmake file that called this function so that it can be referenced later (for example, to 
+	set(SWIG_MODULE_${swigpy_TARGET}_REAL_NAME ${SWIG_MODULE_${swigpy_TARGET}_REAL_NAME} PARENT_SCOPE)
 endfunction()
